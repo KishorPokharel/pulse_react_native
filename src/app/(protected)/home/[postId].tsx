6e3476a34a2f71fd768fc6ff7412fdaabcad8d64
Kishor Pokharel <kishorpokharel7@gmail.com>
2026-02-07 12:17:50 +0545

    Add FullPostView component to display post details with loading state and user avatar

--- src/app/(protected)/home/[postId].tsx
+++ src/app/(protected)/home/[postId].tsx
@@ -1,12 +1,73 @@
+import Avatar from "@/src/components/avatar";
+import FullscreenLoader from "@/src/components/fullscreenLoader";
+import { apiGetSinglePost } from "@/src/http/posts";
+import { formatDate } from "@/src/utils";
+import { useQuery } from "@tanstack/react-query";
 import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Screen() {
-  const params = useLocalSearchParams();
+  const params = useLocalSearchParams<{ postId: string }>();
 
+  return <FullPostView id={+params.postId} />;
+}
+
+type FullPostViewProps = {
+  id: number;
+};
+
+type Post = {
+  id: number;
+  content: string;
+  createdAt: string;
+};
+
+export function FullPostView({ id }: FullPostViewProps) {
+  const { data, isLoading, isError } = useQuery<Post>({
+    queryKey: ["posts", id],
+    queryFn: () => apiGetSinglePost(id),
+  });
+
+  if (isLoading) {
+    return <FullscreenLoader />;
+  }
+
+  const post = data!;
   return (
-    <View>
-      <Text>{JSON.stringify(params)}</Text>
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <ScrollView>
+        <View
+          style={{
+            padding: 16,
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              gap: 8,
+              alignItems: "center",
+            }}
+          >
+            <Avatar name="Kishor" />
+            <Text>Kishor Pokharel • {formatDate(post.createdAt)}</Text>
+          </View>
+          <View
+            style={{
+              marginBlockStart: 8,
+            }}
+          >
+            <Text>{post.content}</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
+}
+
+function PostRepliesView() {
+  return <View></View>;
 }
--- src/http/posts.ts
+++ src/http/posts.ts
@@ -9,3 +9,13 @@ export const apiCreatePost = async (body: { content: string }) => {
   const data = await apiClient.post("/posts", body);
   return data;
 };
+
+export const apiGetSinglePost = async <T>(id: number) => {
+  const data = await apiClient.get<T>("/posts/" + id);
+  return data;
+};
+
+export const apiGetPostChildren = async (id: number) => {
+  const data = await apiClient.get(`/posts/${id}/children`);
+  return data;
+};
