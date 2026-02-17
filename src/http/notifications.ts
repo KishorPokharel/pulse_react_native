d661efb6146beadfcdf71a3b897608ae57021ba0
Kishor Pokharel <kishorpokharel7@gmail.com>
2026-02-17 19:49:27 +0545

    notification apis

--- src/http/notifications.ts
+++ src/http/notifications.ts
@@ -6,3 +6,36 @@ export const apiGetUnreadNotificationCount = async () => {
   );
   return data;
 };
+
+export type GetNotificationResponse = {
+  results: {
+    id: number;
+    type: string;
+    read: boolean;
+    postId: number;
+    createdAt: string;
+    actor: {
+      id: number;
+      name: string;
+    };
+  }[];
+};
+
+export const apiGetNotifications = async () => {
+  const data = await apiClient.get<GetNotificationResponse>("/notifications");
+  return data;
+};
+
+export const apiMarkAllNotificationAsRead = async () => {
+  const data = await apiClient.put<{ success: boolean }>(
+    `/notifications/read-all`,
+  );
+  return data;
+};
+
+export const apiMarkNotificationAsRead = async (notificationId: number) => {
+  const data = await apiClient.put<{ success: boolean }>(
+    `/notifications/${notificationId}/read`,
+  );
+  return data;
+};
