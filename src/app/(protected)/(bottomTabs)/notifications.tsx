b36a860f0896fc0ae9bafe1d7174ce2fa4646e21
Kishor Pokharel <kishorpokharel7@gmail.com>
2026-02-17 19:50:16 +0545

    notification page

--- src/app/(protected)/(bottomTabs)/notifications.tsx
+++ src/app/(protected)/(bottomTabs)/notifications.tsx
@@ -1,16 +1,167 @@
-import { Text, View } from "react-native";
+import Avatar from "@/src/components/avatar";
+import Button from "@/src/components/button";
+import FullscreenLoader from "@/src/components/fullscreenLoader";
+import { useTheme } from "@/src/context/ThemeContext";
+import {
+  useAllNotificationAsRead,
+  useNotificationAsRead,
+  useNotifications,
+} from "@/src/hooks/notifications";
+import { timeAgo } from "@/src/utils";
+import { useRouter } from "expo-router";
+import { FlatList, Pressable, Text, View } from "react-native";
 
 export default function Screen() {
+  const { theme } = useTheme();
+  const { data, isLoading, isRefetching, refetch } = useNotifications();
+
+  const allReadMutation = useAllNotificationAsRead();
+
+  if (isLoading) {
+    return <FullscreenLoader />;
+  }
+
+  const notifications = data?.results || [];
+  if (notifications.length === 0) {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: theme.background,
+        }}
+      >
+        <Text>Nothing to see here.</Text>
+      </View>
+    );
+  }
+
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
+        backgroundColor: theme.background,
       }}
     >
-      <Text>Notifications</Text>
+      <FlatList
+        refreshing={isRefetching}
+        onRefresh={() => refetch()}
+        ItemSeparatorComponent={() => (
+          <View style={{ height: 1, backgroundColor: "#e9e9e97c" }} />
+        )}
+        ListHeaderComponent={
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBlock: 12,
+              paddingInline: 16,
+            }}
+          >
+            <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+              Notifications
+            </Text>
+            <Pressable
+              disabled={allReadMutation.isPending}
+              onPress={() => allReadMutation.mutate()}
+            >
+              <Text>Mark all as Read</Text>
+            </Pressable>
+          </View>
+        }
+        data={notifications}
+        renderItem={({ item: notification }) => (
+          <View
+            style={{
+              paddingBlock: 12,
+              paddingInline: 16,
+              opacity: notification.read ? 0.4 : 1,
+              backgroundColor: theme.background,
+            }}
+          >
+            <NotificationCard notification={notification} />
+          </View>
+        )}
+        keyExtractor={(notification) => notification.id + ""}
+      />
     </View>
   );
+}
+
+type NotificationCardProps = {
+  notification: {
+    id: number;
+    type: string;
+    read: boolean;
+    postId: number;
+    createdAt: string;
+    actor: {
+      id: number;
+      name: string;
+    };
+  };
+};
+function NotificationCard({ notification }: NotificationCardProps) {
+  const { actor } = notification;
+  const router = useRouter();
+
+  const readMutation = useNotificationAsRead();
+
+  return (
+    <>
+      <Pressable
+        onPress={() => {
+          switch (notification.type) {
+            case "follow":
+              router.push({
+                pathname: "/user/[userId]",
+                params: { userId: actor.id },
+              });
+              break;
+            case "comment":
+              router.push({
+                pathname: "/post/[postId]",
+                params: { postId: notification.postId },
+              });
+              break;
+          }
+        }}
+      >
+        <View style={{ flexDirection: "row", gap: 12, alignItems: "center" }}>
+          <Avatar id={actor.id} name={actor.name} />
+          <View>
+            <Text>
+              <Text style={{ fontWeight: "bold" }}>{actor.name}</Text>
+              {getNotificationText(notification.type)}
+            </Text>
+            <Text>{timeAgo(notification.createdAt)}</Text>
+          </View>
+        </View>
+      </Pressable>
+      <View style={{ marginTop: 16 }}>
+        {!notification.read ? (
+          <Button
+            disabled={readMutation.isPending}
+            loading={readMutation.isPending}
+            onPress={() => {
+              readMutation.mutate(notification.id);
+            }}
+            label="Mark as read"
+            type="secondary"
+          />
+        ) : null}
+      </View>
+    </>
+  );
+}
+
+function getNotificationText(type: string) {
+  if (type === "follow") {
+    return " followed you.";
+  }
+  if (type === "comment") {
+    return " commented on a post of yours.";
+  }
+  return "";
 }
--- src/hooks/notifications.ts
+++ src/hooks/notifications.ts
@@ -1,10 +1,86 @@
-import { useQuery } from "@tanstack/react-query";
-import { apiGetUnreadNotificationCount } from "../http/notifications";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Alert } from "react-native";
+import {
+  apiGetNotifications,
+  apiGetUnreadNotificationCount,
+  apiMarkAllNotificationAsRead,
+  apiMarkNotificationAsRead,
+  GetNotificationResponse,
+} from "../http/notifications";
 
 export function useUnreadNotificationCount() {
   return useQuery({
     queryKey: ["notifications", "unread-count"],
     queryFn: apiGetUnreadNotificationCount,
     refetchInterval: 30 * 1000,
+  });
+}
+
+export function useNotifications() {
+  return useQuery({
+    queryKey: ["notifications"],
+    queryFn: apiGetNotifications,
+  });
+}
+
+export function useAllNotificationAsRead() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: apiMarkAllNotificationAsRead,
+    onSuccess: () => {
+      queryClient.setQueryData<GetNotificationResponse>(
+        ["notifications"],
+        (old) => {
+          if (!old) return old;
+          return {
+            ...old,
+            results: old.results.map((not) => {
+              return {
+                ...not,
+                read: true,
+              };
+            }),
+          };
+        },
+      );
+      queryClient.invalidateQueries({
+        queryKey: ["notifications", "unread-count"],
+      });
+    },
+    onError: (err) => {
+      console.log(err);
+      Alert.alert("Something went wrong");
+    },
+  });
+}
+
+export function useNotificationAsRead() {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: apiMarkNotificationAsRead,
+    onSuccess: (data, notificationId) => {
+      queryClient.setQueryData<GetNotificationResponse>(
+        ["notifications"],
+        (old) => {
+          if (!old) return old;
+          return {
+            ...old,
+            results: old.results.map((not) => {
+              return {
+                ...not,
+                read: not.id === notificationId ? true : not.read,
+              };
+            }),
+          };
+        },
+      );
+      queryClient.invalidateQueries({
+        queryKey: ["notifications", "unread-count"],
+      });
+    },
+    onError: (err) => {
+      console.log(err);
+      Alert.alert("Something went wrong");
+    },
   });
 }
